cb7bcfbd8a26d7f99ce7289ffa0c9ef90dd67226
vadymshukr <vadymshukr@gmail.com>
2020-01-28 19:20:00 +0200

    Few bugs fixed, project almost completed

--- js/script.js
+++ js/script.js
@@ -6,4 +6,13 @@ $( document ).ready(function() {
             $( this ).removeClass( 'navbar__link__active' )
         }
     )
+
+    $( '.color-circle' ).hover(
+        function(){
+            $( '.color-circle__active').removeClass('color-circle__active')
+            $( this ).addClass( 'color-circle__active' )
+        }, function(){
+            $( this ).removeClass( 'color-circle__active' )
+        }
+    )
 });
